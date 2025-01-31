6109a6ba4995a82f8288bf0a11886d17126bb8da
Matteo Zagheno <matteo2309@hotmail.com>
2025-01-31 17:05:17 +0100

    GLDEBUGPROC message callback fix

--- src/renderer.h
+++ src/renderer.h
@@ -10,6 +10,10 @@
 #include <gpuobjects/shader.h>
 #include <gpuobjects/texture.h>
 
+void message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
+                      const GLchar* message,
+                      const void* user_param);
+
 class Renderer
 {
 public:
@@ -48,7 +52,7 @@ public:
 
         glEnable(GL_DEBUG_OUTPUT);
         glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-        glDebugMessageCallback(message_callback, nullptr);
+        glDebugMessageCallback(static_cast<GLDEBUGPROC>(message_callback), nullptr);
 
         int width, height;
         glfwGetFramebufferSize(_window, &width, &height);
@@ -203,51 +207,51 @@ private:
     //TODO: change implementation to something like unordered_map<pair/tuple, value>
     std::unordered_map<string, unique_ptr<Texture const>> _textures;
     std::vector<function<void()>> _pipeline;
-
-    static void message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
-                                 GLchar const* message,
-                                 void const* user_param)
-    {
-        auto const src_str = [source]()
-        {
-            switch (source)
-            {
-            case GL_DEBUG_SOURCE_API: return "API";
-            case GL_DEBUG_SOURCE_WINDOW_SYSTEM: return "WINDOW SYSTEM";
-            case GL_DEBUG_SOURCE_SHADER_COMPILER: return "SHADER COMPILER";
-            case GL_DEBUG_SOURCE_THIRD_PARTY: return "THIRD PARTY";
-            case GL_DEBUG_SOURCE_APPLICATION: return "APPLICATION";
-            case GL_DEBUG_SOURCE_OTHER: return "OTHER";
-            default: return "GL_DEBUG source Enum not recognized";
-            }
-        }();
-
-        auto const type_str = [type]()
-        {
-            switch (type)
-            {
-            case GL_DEBUG_TYPE_ERROR: return "ERROR";
-            case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: return "DEPRECATED_BEHAVIOR";
-            case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: return "UNDEFINED_BEHAVIOR";
-            case GL_DEBUG_TYPE_PORTABILITY: return "PORTABILITY";
-            case GL_DEBUG_TYPE_PERFORMANCE: return "PERFORMANCE";
-            case GL_DEBUG_TYPE_MARKER: return "MARKER";
-            case GL_DEBUG_TYPE_OTHER: return "OTHER";
-            default: return "GL_DEBUG type Enum not recognized";
-            }
-        }();
-
-        auto const severity_str = [severity]()
-        {
-            switch (severity)
-            {
-            case GL_DEBUG_SEVERITY_NOTIFICATION: return "NOTIFICATION";
-            case GL_DEBUG_SEVERITY_LOW: return "LOW";
-            case GL_DEBUG_SEVERITY_MEDIUM: return "MEDIUM";
-            case GL_DEBUG_SEVERITY_HIGH: return "HIGH";
-            default: return "GL_DEBUG severity Enum not recognized";
-            }
-        }();
-        std::cout << src_str << ", " << type_str << ", " << severity_str << ", " << id << ": " << message << '\n';
-    }
 };
+
+inline void message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
+                      const GLchar* message,
+                      const void* user_param)
+{
+    auto const src_str = [source]()
+    {
+        switch (source)
+        {
+        case GL_DEBUG_SOURCE_API: return "API";
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM: return "WINDOW SYSTEM";
+        case GL_DEBUG_SOURCE_SHADER_COMPILER: return "SHADER COMPILER";
+        case GL_DEBUG_SOURCE_THIRD_PARTY: return "THIRD PARTY";
+        case GL_DEBUG_SOURCE_APPLICATION: return "APPLICATION";
+        case GL_DEBUG_SOURCE_OTHER: return "OTHER";
+        default: return "GL_DEBUG source Enum not recognized";
+        }
+    }();
+
+    auto const type_str = [type]()
+    {
+        switch (type)
+        {
+        case GL_DEBUG_TYPE_ERROR: return "ERROR";
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: return "DEPRECATED_BEHAVIOR";
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: return "UNDEFINED_BEHAVIOR";
+        case GL_DEBUG_TYPE_PORTABILITY: return "PORTABILITY";
+        case GL_DEBUG_TYPE_PERFORMANCE: return "PERFORMANCE";
+        case GL_DEBUG_TYPE_MARKER: return "MARKER";
+        case GL_DEBUG_TYPE_OTHER: return "OTHER";
+        default: return "GL_DEBUG type Enum not recognized";
+        }
+    }();
+
+    auto const severity_str = [severity]()
+    {
+        switch (severity)
+        {
+        case GL_DEBUG_SEVERITY_NOTIFICATION: return "NOTIFICATION";
+        case GL_DEBUG_SEVERITY_LOW: return "LOW";
+        case GL_DEBUG_SEVERITY_MEDIUM: return "MEDIUM";
+        case GL_DEBUG_SEVERITY_HIGH: return "HIGH";
+        default: return "GL_DEBUG severity Enum not recognized";
+        }
+    }();
+    std::cout << src_str << ", " << type_str << ", " << severity_str << ", " << id << ": " << message << '\n';
+}
