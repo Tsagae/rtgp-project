d4396c5f31080f539357a1f27951828156a3004b
Matteo Zagheno <43522772+Tsagae@users.noreply.github.com>
2025-03-01 23:40:19 +0100

    changed names of benchmarks

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -453,14 +453,14 @@ BENCHMARK(BM_CopyFrameBuffer)->Args({800, 600})->Args({1280, 720})->Args({1920, 
 BENCHMARK(BM_ReadFrameBuffer)->Args({800, 600})->Args({1280, 720})->Args({1920, 1080})->Setup(DoSetup)->
                                Teardown(DoTearDown)->Unit(benchmark::kMillisecond);
 BENCHMARK(BM_Pipeline_Step_1)->
-Name("Pipeline step 1: draw particles pixels to off-screen buffer (screen w/screen h/particle buf w/particle buf h)")->
+Name("BM_Pipeline_Step_1: draw particles pixels to off-screen buffer (screen w/screen h/particle buf w/particle buf h)")->
 Args({800, 600})->Args({1280, 720})->Args({1920, 1080})->Setup(DoSetup)->Teardown(DoTearDown)->Unit(
                                  benchmark::kMillisecond);
-BENCHMARK(BM_Pipeline_Step_2)->Name("Pipeline step 2: draw disappearing model (screen w/screen h)")->
+BENCHMARK(BM_Pipeline_Step_2)->Name("BM_Pipeline_Step_2: draw disappearing model (screen w/screen h)")->
                                Args({800, 600})->Args({1280, 720})->Args({1920, 1080})->
                                Setup(DoSetup)->Teardown(DoTearDown)->Unit(benchmark::kMillisecond);
 BENCHMARK(BM_Pipeline_Step_3)->Name(
-                                 "Pipeline step 3: read off-screen buffer and spawn particles (screen w/screen h/particle buf w/particle buf h)")
+                                 "BM_Pipeline_Step_3: read off-screen buffer and spawn particles (screen w/screen h/particle buf w/particle buf h)")
                              ->
                              ArgsProduct({
                                  {1920},
@@ -486,7 +486,7 @@ BENCHMARK(BM_Pipeline_Step_3)->Name(
                              })->Setup(DoSetup)->Teardown(DoTearDown)->Unit(
                                  benchmark::kMillisecond);
 BENCHMARK(BM_Pipeline_Step_4)->Name(
-    "Pipeline step 4: draw particles (screen w/screen h/particle buf w/particle buf h)")->ArgsProduct({
+    "BM_Pipeline_Step_4: draw particles (screen w/screen h/particle buf w/particle buf h)")->ArgsProduct({
     {1920},
     {1080},
     {800},
@@ -510,7 +510,7 @@ BENCHMARK(BM_Pipeline_Step_4)->Name(
 })->Setup(DoSetup)->Teardown(DoTearDown)->Unit(
     benchmark::kMillisecond);
 BENCHMARK(BM_Pipeline_Complete)->Name(
-    "Complete pipeline: (screen w/screen h/particle buf w/particle buf h)")->ArgsProduct({
+    "BM_Pipeline_Complete: (screen w/screen h/particle buf w/particle buf h)")->ArgsProduct({
     {1920},
     {1080},
     {800},
